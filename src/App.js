668b818c5322bcefa0cf3159fd72e1049e040cd7
zeredogo <zeredogo@gmail.com>
2022-08-11 22:21:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Modal from './Modal';
+import Sidebar from './Sidebar';
+import Home from './Home';
+function App() {
+  return (
+    <>
+      <h2>modal-sidebar project setup</h2>
+    </>
+  )
+}
+
+export default App
