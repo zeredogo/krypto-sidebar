c64c8e662c019bb90d4ea80ca5feb9be8f7fe5ba
zeredogo <zeredogo@gmail.com>
2022-08-11 23:57:52 +0100

    updating modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import { AppProvider } from './context';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <AppProvider>
+       <App />
+    </AppProvider>, 
+document.getElementById('root')
+);
